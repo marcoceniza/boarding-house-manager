4f0173a79beeafdf06b2b86cd9b1be0b04a50cf4
Marco Ceniza <marcoceniza15@gmail.com>
2026-01-27 22:33:25 +0800

    Added Register page

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,14 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/login">Login</RouterLink>
       </nav>
     </div>
-  </header>
+  </header> -->
 
   <RouterView />
 </template>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -8,10 +8,14 @@ import BaseButton from '@/components/BaseButton.vue';
     <div class="min-h-screen flex items-center justify-center px-4 bg-gradient-to-br from-amber-50 via-stone-100 to-rose-50">
         <div class="w-full max-w-md mx-auto p-8 rounded-2xl bg-white/90 backdrop-blur shadow-2xl">
             <div class="text-center mb-8">
-                <h1 class="text-3xl font-extrabold text-gray-800">Welcome Back</h1>
-                <p class="text-gray-500 mt-2">Please login to your account</p>
+                <h1 class="text-3xl font-extrabold text-gray-800">Create an Account</h1>
+                <p class="text-gray-500 mt-2">Sign up to get started</p>
             </div>
-            <form @submit.prevent="login" class="space-y-5">
+            <form @submit.prevent="register" class="space-y-5">
+                <BaseInput
+                    label="Name"
+                    placeholder="Name"
+                />
                 <BaseInput
                     type="email"
                     label="Email"
@@ -22,16 +26,16 @@ import BaseButton from '@/components/BaseButton.vue';
                     label="Password"
                     placeholder="••••••••"
                 />
-                <div class="flex items-center justify-between text-sm">
-                    <label class="flex items-center gap-2 text-gray-600">
-                    <input type="checkbox" class="rounded text-indigo-600 focus:ring-indigo-500">Remember me</label>
-                    <a href="#" class="text-indigo-600 hover:underline">Forgot password?</a>
-                </div>
-                <BaseButton type="submit" rounded fullWidth>LOGIN</BaseButton>
+                <BaseInput
+                    type="password"
+                    label="Confirm Password"
+                    placeholder="••••••••"
+                />
+                <BaseButton type="submit" rounded fullWidth>CREATE ACCOUNT</BaseButton>
             </form>
             <p class="text-center text-sm text-gray-600 mt-6">
-                Don’t have an account?
-                <a @click="$router.push('/register')"  class="text-indigo-600 cursor-pointer font-semibold hover:underline">Sign up</a>
+                Already have an account?
+                <a @click="$router.push('/login')" class="text-indigo-600 cursor-pointer font-semibold hover:underline">Log in</a>
             </p>
         </div>
     </div>
